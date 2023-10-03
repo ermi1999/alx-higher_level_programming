6f222e071393ae5279125ef0dd67418725a6490c
ermi1999 <ermiyasabiye1999@gmail.com>
2023-10-03 08:25:48 -0700

    task 6

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,6 @@ for i in range(0, 100):
     if i < 10:
         i = "0" + str(i)
     if i == 99:
-        print("{}\n".format(i))
+        print("{}".format(i))
     else:
         print("{}".format(i), end=", ")
