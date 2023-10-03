6e5a08dd0ec160118a34232b39a0b9f6c9f9a16f
ermi1999 <ermiyasabiye1999@gmail.com>
2023-10-03 09:35:02 -0700

    task 8

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    lower = 0
-    for i in range(97, 123):
-        if chr(i) == c:
-            lower = 1
-    if lower == 1:
-        return True
-    else:
-        return False
+    return ord(c) >= 97 and ord(c) <= 122
