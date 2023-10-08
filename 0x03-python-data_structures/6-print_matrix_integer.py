b16ec101533759487594de907eb3627ad65ad237
ermi1999 <ermiyasabiye1999@gmail.com>
2023-10-08 02:09:38 -0700

    task 7

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        k = 0
+        for j in i:
+            if k == len(i) - 1:
+                print("{:d}".format(j), end="")
+                k += 1
+            else:
+                print("{:d}".format(j), end=" ")
+                k += 1
+        print()
