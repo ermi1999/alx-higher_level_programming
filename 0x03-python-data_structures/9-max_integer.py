504f6c6e32f8f0446856e40f54f101f04a8c18cf
ermi1999 <ermiyasabiye1999@gmail.com>
2023-10-08 05:24:05 -0700

    task 10

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) < 1:
+        return None
+    else:
+        result = 0
+        for i in my_list:
+            if i > result:
+                result = i
+        return result
