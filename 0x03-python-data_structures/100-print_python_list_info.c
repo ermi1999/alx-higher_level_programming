b996abdcc57ef2f48a473add8dc443eaf14c2406
ermi1999 <ermiyasabiye1999@gmail.com>
2023-10-09 05:25:37 -0700

    task 15

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include <Python.h>
+/*
+ * print_python_list_info - Prints information about a Python list
+ * @p: Pointer to the Python list object
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t list_size = PyList_Size(p);
+	Py_ssize_t allocated_s = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %zd\n", list_size);
+	printf("[*] Allocated = %zd\n", allocated_s);
+	Py_ssize_t i;
+
+	for (i = 0; i < list_size; i++)
+	{
+		const char *element_name;
+
+		PyObject *element = PyList_GetItem(p, i);
+		PyObject *element_type = PyObject_Type(element);
+
+		element_name = Py_TYPE(element_type)->tp_name;
+		printf("Element %ld: %s\n", i, element_name);
+		Py_DECREF(element_type);
+	}
+}
+
