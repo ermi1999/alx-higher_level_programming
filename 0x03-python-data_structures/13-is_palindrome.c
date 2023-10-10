2bc04fc7c0cef386d9a994eaa55978ceb69248e2
ermi1999 <ermiyasabiye1999@gmail.com>
2023-10-09 23:54:10 -0700

    fixed mixed declaration

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,12 +8,10 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
+	listint_t *prev = NULL;                                         listint_t *current = *head;                                     listint_t *next = NULL;
+
 	if (*head == NULL || (*head)->next == NULL)
 		return (*head);
-
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next = NULL;
 
 	while (current != NULL)
 	{
@@ -59,7 +57,7 @@ int compare_lists(listint_t *list1, listint_t *list2)
 	while (list1 != NULL && list2 != NULL)
 	{
 		if (list1->n != list2->n)
-			return 0;
+			return (0);
 
 		list1 = list1->next;
 		list2 = list2->next;
@@ -83,15 +81,40 @@ int is_palindrome(listint_t **head)
 	listint_t *mid = NULL;
 	listint_t *second_half = NULL;
 
+	/* Move the variable declarations to the beginning of the function */
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
 	mid = get_middle(*head);
 	second_half = mid->next;
 	mid->next = NULL;
 
-	reverse_listint(&second_half);
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
 
 	int is_palindrome = compare_lists(*head, second_half);
 
-	reverse_listint(&second_half);
+	/* Restore the original list */
+	current = *head;
+	prev = NULL;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
 	mid->next = second_half;
 
 	return (is_palindrome);
