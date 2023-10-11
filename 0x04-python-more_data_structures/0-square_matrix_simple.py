b4185e6877493f92855ca0a689e4ec8bb0e3fb39
ermi1999 <ermiyasabiye1999@gmail.com>
2023-10-11 04:49:33 -0700

    task 1

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    import copy
+    my_list = []
+    i = 0;
+    for i, row in enumerate(matrix):
+        copy = []
+        for j, element in enumerate(row):
+            num = element ** 2
+            copy.append(num)
+        my_list.append(copy)
+    return my_list
