abdde42c4a92354411280b239259b9e1e2be6e3e
ermi1999 <ermiyasabiye1999@gmail.com>
2023-10-11 08:25:38 -0700

    task 12

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    copy = my_list
-    result = list(map(lambda x: x * number, copy))
-    return result
+    return list(map(lambda x: x * number, my_list.copy()))
