340645e4825db94406c0af6cbd8352b7ff52278a
ermi1999 <ermiyasabiye1999@gmail.com>
2023-10-11 12:20:10 -0700

    task 14

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    list1 = []
+    add1 = 0
+    result = 0
+    for i in my_list:
+        list1.append(i[0] * i[1])
+
+    for j in list1:
+        add1 += j
+
+    for k in my_list:
+        result += k[1]
+
+    result = add1 / result
+    return result
