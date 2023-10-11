43c23652ff2e9cc5c1dbe67c22698e613c6972e2
ermi1999 <ermiyasabiye1999@gmail.com>
2023-10-11 10:20:43 -0700

    task 13

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     result = 0
-    if not roman_string or roman_string == str:
-        return None
-    else:
-        for i in roman_string:
-            if i == "M":
-                result += 1000
-            elif i == "D":
-                result += 500
-            elif i == "C":
-                result += 100
-            elif i == "L":
-                result += 50
-            elif i == "X":
-                result += 10
-            elif i == "V":
-                result += 5
-            elif i == "I":
-                result += 1
-    return result
 
+    if not roman_string or not isinstance(roman_string, str):
+        return result
+
+    roman_values = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
+
+    for i in range(len(roman_string)):
+        if (
+                i + 1 < len(roman_string) and
+                roman_values[roman_string[i]] <
+                roman_values[roman_string[i + 1]]
+                ):
+            result -= roman_values[roman_string[i]]
+        else:
+            result += roman_values[roman_string[i]]
+
+    return result
