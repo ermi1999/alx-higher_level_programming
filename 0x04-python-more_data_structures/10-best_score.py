a47590814a47c1ad9084bdf089b233d46fe70a3e
ermi1999 <ermiyasabiye1999@gmail.com>
2023-10-11 08:05:19 -0700

    task 11

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     best = 0
     key = ""
-    if a_dictionary == None:
+    if not a_dictionary:
         return None
     for i in a_dictionary:
         if a_dictionary[i] > best:
