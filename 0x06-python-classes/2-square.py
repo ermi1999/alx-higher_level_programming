85b4149aedf4da52e37375a4e1326f45f3a6b659
ermi1999 <ermiyasabiye1999@gmail.com>
2023-10-24 08:10:11 -0700

    task 3

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" defines a Private instance attribute: size """
+
+
+class Square:
+    """
+    defines a Private instance attribute size
+
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a Square object with a given size.
+        Args:
+            size The size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
