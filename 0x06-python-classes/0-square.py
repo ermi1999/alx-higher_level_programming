4ad90d419799fa8f29a9b399caf0c0c906608c51
ermi1999 <ermiyasabiye1999@gmail.com>
2023-10-24 07:38:46 -0700

    task 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,10 @@
-"""This is an empty class Square that defines a square """
 #!/usr/bin/python3
+""" This is an empty class Square that defines a square. """
+
+
 class Square:
-    """ 
-    Does nothing 
+    """
+    Does nothing
 
     Attributes:
         No attributes are defined for this class.
