40f0843259551e6ee8df3702540fba59320ccd7b
ermi1999 <ermiyasabiye1999@gmail.com>
2023-11-06 08:05:53 -0800

    task 6

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+this program initializes a class called BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    this class initializes a class called BaseGeometry.
+    """
+    pass
