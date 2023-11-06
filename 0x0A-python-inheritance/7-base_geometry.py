35bff39f42bdf9d58ca48a684239bfe562237bcd
ermi1999 <ermiyasabiye1999@gmail.com>
2023-11-06 09:06:52 -0800

    task 8

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This program validets an integer.
+"""
+
+
+class BaseGeometry:
+    """
+    Initializes a class called BaseGeometry.
+    """
+    def area(self):
+        """
+        Raises an exceprion when it is called.
+        """
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """
+        This function validates an integer.
+        Args:
+            name: the name of the integer.
+            value: the integer.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/7-base_geometry.txt")
