8c8a5951fcfb4c12b00a404f871ad1e98570003e
ermi1999 <ermiyasabiye1999@gmail.com>
2023-11-06 10:48:49 -0800

    task 12

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This program intializes the class square.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    This class intializes the object square.
+    Args:
+        Rectangle: The class that the object inherits from.
+    """
+    def __init__(self, size):
+        """
+        This function initializes size.
+        Args:
+            size: The size.
+        """
+        self.integer_validator("size", size)
+
+        self.__size = size
+
+    def area(self):
+        """
+        This function returns the area
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        This function Returns a string.
+        """
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
