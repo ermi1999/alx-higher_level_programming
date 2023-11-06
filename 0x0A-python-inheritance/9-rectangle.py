fc13fa58aa4d3403da4a005efae554f30a91ba70
ermi1999 <ermiyasabiye1999@gmail.com>
2023-11-06 10:31:29 -0800

    task 10

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This program Initializes a class that is inherited from BaseGeometry.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Initializes a class that is inherited from BaseGeometry.
+    Args:
+        BaseGeometry: The class that is inherited from.
+    """
+    def __init__(self, width, height):
+        """
+        This function initializes width and height.
+        Args:
+            width: The width
+            height: The height.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """
+        This function returns the area
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        This function Returns a string.
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
