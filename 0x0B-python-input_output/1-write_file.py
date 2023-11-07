e7ae4ed044544db773d03d25a5e45e7ccad1b641
ermi1999 <ermiyasabiye1999@gmail.com>
2023-11-07 03:46:51 -0800

    task 2

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This program writes a string into a text fileand returns the number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    This function writes a string into a text file.
+    Args:
+        filename: The name of the file to be created.
+        text: the text to be written.
+    """
+    with open(filename, mode='w', encoding="utf-8") as f:
+        return f.write(text)
