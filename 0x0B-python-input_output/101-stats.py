81205649337a4d7838abdf4e873a7ee089aad4a1
ermi1999 <ermiyasabiye1999@gmail.com>
2023-11-08 08:13:59 -0800

    task 15

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,30 +3,36 @@
 This program reads stdin line by line and computes metrics.
 """
 
+import sys
+
 def stats():
     """
     This program reads stdin line by line and computes metrics.
     """
-    status_code = {
-    '200': 0, '301': 0, '400': 0, '401': 0,
-    '403': 0, '404': 0, '405': 0, '500': 0
-    }
-    file_size = 0
     lines = 10
+
     try:
-        while (True):
-            i = 0
-            while (i < lines):
-                line = input()
+        while True:
+            status_code = {
+                '200': 0, '301': 0, '400': 0, '401': 0,
+                '403': 0, '404': 0, '405': 0, '500': 0
+            }
+            file_size = 0
+
+            for i in range(lines):
+                line = sys.stdin.readline().strip()
                 words = line.split()
                 status_code[words[7]] += 1
                 file_size += int(words[8])
-                i += 1
+
             print("File size: {:d}".format(file_size))
             sort = sorted(status_code.keys())
             for key in sort:
                 if status_code[key] > 0:
                     print("{:d}: {:d}".format(int(key), status_code[key]))
+
+            lines += 10
+
     except KeyboardInterrupt:
         print("File size: {:d}".format(file_size))
         sort = sorted(status_code.keys())
@@ -34,5 +40,6 @@ def stats():
             if status_code[key] > 0:
                 print("{:d}: {:d}".format(int(key), status_code[key]))
         raise
+
 if __name__ == "__main__":
     stats()
