c59c6d6c9a7b5908cb60ddbf2c4ecc42aca38319
ermi1999 <ermiyasabiye1999@gmail.com>
2023-11-08 09:41:28 -0800

    task 15

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,43 +2,53 @@
 """
 This program reads stdin line by line and computes metrics.
 """
-
 import sys
+
+
+
+def print_stats(status_code, size):
+    """
+    This function handle the printing of the stats.
+    """
+    print("File size: {}".format(size))
+    sort = sorted(status_code.keys())
+    for key in sort:
+        if status_code[key] > 0:
+            print("{}: {}".format(key, status_code[key]))
+
 
 def stats():
     """
-    This program reads stdin line by line and computes metrics.
+    This function reads stdin line by line and computes metrics.
     """
-    lines = 10
-
+    lines = 0
+    size = 0
+    status_code = {
+        '200': 0, '301': 0, '400': 0, '401': 0,
+        '403': 0, '404': 0, '405': 0, '500': 0
+    }
     try:
-        status_code = {
-            '200': 0, '301': 0, '400': 0, '401': 0,
-            '403': 0, '404': 0, '405': 0, '500': 0
-        }
-        file_size = 0
+        for line in sys.stdin:
+            if lines == 10:
+                print_stats(status_code, size)
+                lines = 1
+            else:
+                lines += 1
+            line = line.split()
+            try:
+                size += int(line[8])
+            except (IndexError, ValueError):
+                pass
+            try:
+                if line[7] in list(status_code.keys()):
+                    status_code[line[7]] += 1
+            except IndexError:
+                pass
 
-        while True:
-            for i in range(lines):
-                line = sys.stdin.readline().strip()
-                words = line.split()
-                status_code[words[7]] += 1
-                file_size += int(words[8])
-
-            print("File size: {}".format(file_size))
-            sort = sorted(status_code.keys())
-            for key in sort:
-                if status_code[key] > 0:
-                    print("{}: {}".format(key, status_code[key]))
-
-            lines += 10
+        print_stats(status_code, size)
 
     except KeyboardInterrupt:
-        print("File size: {}".format(file_size))
-        sort = sorted(status_code.keys())
-        for key in sort:
-            if status_code[key] > 0:
-                print("{}: {}".format(key, status_code[key]))
+        print_stats(status_code, size)
         raise
 
 if __name__ == "__main__":
