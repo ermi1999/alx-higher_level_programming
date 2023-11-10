9d51063e3d863ec5593636d5f71b51b30f567236
ermi1999 <ermiyasabiye1999@gamail.com>
2023-11-10 08:43:13 -0800

    half way through

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 This program intializes a class called Base.
 """
+import json
 
 
 class Base:
@@ -21,3 +22,26 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @classmethod
+    def to_json_string(cls, list_dictionaries):
+        """
+        This function returns the JSON string representation
+        of list_dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        This function writes JSON string representation of list_objs to a file
+        """
+        dicts = []
+        file_name = "{}.json".format(cls.__name__)
+        for obj in list_objs:
+            dicts.append(obj.to_dictionary())
+        json_str = Base.to_json_string(dicts)
+        with open(file_name, "w") as f:
+            f.write(json_str)
