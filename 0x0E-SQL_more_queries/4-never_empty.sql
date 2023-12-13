db94ab1cf12cbc3406dfc63bc430669228dd0359
ermi1999 <ermiyasabiye1999@gmail.com>
2023-12-13 06:03:56 -0800

    task 5

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- creates the table id_not_null on your MySQL server.
+-- with a values (id) INT and name VARCHAR(256) id has a default value of 1.
+CREATE TABLE IF NOT EXISTS id_not_null(
+	id INT DEFAULT 1,
+	name VARCHAR(256)
+)
