ee00fb78d822fa69a19c2be01ccac40e9c8e8a27
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-12 17:36:18 +0300

    safe filter

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+    Args:
+        Arg1: mysql username.
+        Arg2: mysql password.
+        Arg3: database name
+        Arg4: state name searched
+"""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = BINARY (%s)",
+                (sys.argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
