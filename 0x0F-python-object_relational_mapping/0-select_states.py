169dd294a6649e5f24019be8e8dc4720493a8f6f
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-12 16:15:21 +0300

    get all states fixed styling

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,12 +9,13 @@
 """
 import MySQLdb
 import sys
-conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                       passwd=sys.argv[2], db=sys.argv[3])
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
