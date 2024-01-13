40a7bf68367b0953cf504c38c113f6238116ff7c
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-13 12:43:36 +0300

    get state id

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+this program prints the State object with the name passed as argument from the database hbtn_0e_6_usa.
+"""
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]
+    ))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    instance = session.query(State).filter(State.name == '{}'.format(sys.argv[4]))
+    if instance:
+        print("{:d}".format(instance[0].id))
+    else:
+        print("Not found")
+    session.close()
