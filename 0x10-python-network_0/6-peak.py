a3369f98feaed591809a0293d0d88de1b249a32b
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-25 18:15:44 +0300

    find the peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Finds the peak number in a list"""
+
+
+def find_peak(list_of_integers):
+    """Finds the peak number in a list"""
+    if len(list_of_integers) == 0:
+        return None
+    peaks = []
+    i = 0
+    while i < len(list_of_integers) - 1:
+        if list_of_integers[i] >= list_of_integers[i - 1] and \
+                list_of_integers[i] >= list_of_integers[i + 1]:
+            peaks.append(list_of_integers[i])
+            i += 2
+        else:
+            i += 1
+    return max(peaks)
