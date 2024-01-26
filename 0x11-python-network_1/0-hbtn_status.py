67cb65136e23f028d8060ad8a803dd0c7e61bfc9
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-26 11:34:42 +0300

    fetch the status of hbtn

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this module fetches a url and prints the information."""
+import urllib.request
+
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    content = response.read()
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(
+        type(content), content, content.decode('utf-8')
+    ))
