43f4990a18a450905838f2f26eb7db79eb506210
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-26 03:45:07 -0800

    json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""this module takes a letter as argument and sends a post request."""
+import sys
+import requests
+
+if __name__ == "__main__":
+    q = ""
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    req = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        js = req.json()
+        if len(js) > 0:
+            print("[{}] {}".format(js.get('id'), js.get('name')))
+        else:
+            print("No result")
+    except requests.exceptions.JSONDecodeError as e:
+        print("Not a valid JSON")
