e90d2f9d2a35a7a0d2d865cb034306057237e2c1
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-26 13:25:06 +0300

    get request with requests module

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,6 @@
 import requests
 
 if __name__ == "__main__":
-    req = requests.get("https://alx-intranet.hbtn.io/status")
-    print("Body response:\n\t-type: {}\n\t-content: {}".format(
-        type(req.text), req.text
-    ))
+    with requests.get("https://alx-intranet.hbtn.io/status") as req:
+        print("Body response:\n\t-type: {}\n\t-content: {}".format(
+              type(req.text), req.text))
