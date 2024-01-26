4237db0cb35cd89dce2e28dffacc5a30cc7689c3
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-26 03:04:30 -0800

    fixed post request

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,8 +3,8 @@
 url are passed as an argument
 """
 import sys
-import responses
+import requests
 
 if __name__ == "__main__":
-    res = responses.post(sys.argv[1], data={'email': sys.argv[2]})
+    res = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     print(res.text)
