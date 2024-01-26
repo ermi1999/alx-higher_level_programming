57710546835b466ce638ca7fcbc190cca917e1c1
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-26 12:12:12 +0300

    fetch a header from the url

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """get a value from the header"""
 import sys
-from urllib import request
+import urllib.request
 
-with request.urlopen(sys.argv[1]) as response:
+with urllib.request.urlopen(sys.argv[1]) as response:
     print(response.getheader('X-Request-Id'))
