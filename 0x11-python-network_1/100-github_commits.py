16abb76a08a08ceffb397d5dcc7e5daff3cd6f55
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-26 18:42:57 +0300

    fetch commits from github api

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,7 +5,7 @@ import requests
 
 if __name__ == "__main__":
     res = requests.get(
-        'https://api.github.com/repos/{}/{}/commits?limit=10'.format(
+        'https://api.github.com/repos/{}/{}/commits'.format(
             sys.argv[2], sys.argv[1]))
     objs = res.json()
     for i in range(10):
