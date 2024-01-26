4b4490a7b3e7a5646266eda6ee79147eb3201136
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-26 15:25:58 +0300

    fetch githubs api

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""this module takes github credentials
+as an argument (username) and (password) and prints their id"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    res = requests.get('https://api.github.com/user', auth=auth)
+    print(res.json().get('id'))
