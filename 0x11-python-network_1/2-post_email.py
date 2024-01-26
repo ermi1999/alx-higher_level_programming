8eaacd876d749fe978fa8ee7a7221bd3eaee7e85
Ermiyas <ermiyasabiye1999@gmail.com>
2024-01-26 12:55:40 +0300

    post reques with parameters

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""accepts url and email as an argument and sends a post request"""
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({'email': sys.argv[2]})
+    data = data.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as res:
+        print(res.read().decode('utf-8')
