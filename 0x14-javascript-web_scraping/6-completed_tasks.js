b92bc33122946aae0bb4606a0175989a699ca767
Ermiyas <ermiyasabiye1999@gmail.com>
2024-02-20 21:33:04 +0300

    compeleted tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,18 +3,21 @@
 const request = require('request');
 
 request(process.argv[2], function (err, response, body) {
+  if (err) {
+    console.log(err);
+    return;
+  }
   body = JSON.parse(body);
-  const result = {}
+  const result = {};
 
   for (let i = 0; i < body.length; i++) {
     if (body[i].completed) {
       if (result[body[i].userId]) {
         result[body[i].userId]++;
-      }
-      else {
+      } else {
         result[body[i].userId] = 1;
       }
     }
   }
   console.log(result);
-})
+});
