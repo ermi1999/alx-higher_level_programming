b45216108c0b392feb3b04bc397fe6f2cfcf16dc
Ermiyas <ermiyasabiye1999@gmail.com>
2024-02-20 20:51:04 +0300

    star wars character

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
 
-let count = 0;
-request(process.argv[2], function (err, res, body) {
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, function (err, res, body) {
   if (err) {
     console.log(err);
   }
-  const results = JSON.parse(body).results;
-  for (let i = 0; i < results.length; i++) {
-    const characters = results[i].characters;
-    if (characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-      count++;
-    }
-  }
-  console.log(count);
+  const _body = JSON.parse(body);
+  console.log(_body.title);
 });
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, function (err, res, body) {
+let count = 0;
+request(process.argv[2], function (err, res, body) {
   if (err) {
     console.log(err);
   }
-  const _body = JSON.parse(body);
-  console.log(_body.title);
+  const results = JSON.parse(body).results;
+  for (let i = 0; i < results.length; i++) {
+    const characters = results[i].characters;
+    if (characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+      count++;
+    }
+  }
+  console.log(count);
 });
